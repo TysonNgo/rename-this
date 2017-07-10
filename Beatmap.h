a79b7d6399011809cf80619609d1ee7da2145aef
Tyson <tyson_ngo@hotmail.com>
2017-07-10 09:45:24 -0400

    added sample beatmaps with varying osu file formats (v03-v14)

--- Beatmap.h
+++ Beatmap.h
@@ -1,1 +1,21 @@
+#ifndef BEATMAP_H
+#define BEATMAP_H
 
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "HitObject.h"
+#include "TimingPoint.h"
+
+class Beatmap {
+    private:
+        float ar, cs, od, hp;
+        std::vector<HitObject> hitObjects;
+        std::vector<TimingPoint> timingPoints;
+    public:
+        Beatmap(std::string b);
+        Beatmap(std::ifstream &b);
+};
+
+#endif
