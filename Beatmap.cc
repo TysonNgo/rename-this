9ad511c831a21d33cf3b5db33ae2c51aeba91114
tysonngo <tyson_ngo@hotmail.com>
2017-07-24 13:11:50 -0400

    added empty implementation file

---
+++ Beatmap.cc
@@ -0,0 +1,221 @@
+#include "Beatmap.h"
+
+using namespace std;
+
+Beatmap::Beatmap(string b){
+
+};
+
+Beatmap::Beatmap(ifstream &b){
+
+};
+
+int Beatmap::getOsuFileFormat(){
+
+}
+string Beatmap::getAudioFilename(){
+
+}
+int Beatmap::getAudioLeadIn(){
+
+}
+int Beatmap::getPreviewTime(){
+
+}
+int Beatmap::getCountdown(){
+
+}
+string Beatmap::getSampleSet(){
+
+}
+float Beatmap::getStackLeniency(){
+
+}
+int Beatmap::getMode(){
+
+}
+int Beatmap::getLetterboxInBreaks(){
+
+}
+int Beatmap::getWidescreenStoryboard(){
+
+}
+vector<int> Beatmap::getBookmarks(){
+
+}
+float Beatmap::getDistanceSpacing(){
+
+}
+int Beatmap::getBeatDivisor(){
+
+}
+int Beatmap::getGridSize(){
+
+}
+int Beatmap::getTimelineZoom(){
+
+}
+string Beatmap::getTitle(){
+
+}
+string Beatmap::getTitleUnicode(){
+
+}
+string Beatmap::getArtist(){
+
+}
+string Beatmap::getArtistUnicode(){
+
+}
+string Beatmap::getCreator(){
+
+}
+string Beatmap::getVersion(){
+
+}
+string Beatmap::getSource(){
+
+}
+string Beatmap::getTags(){
+
+}
+string Beatmap::getBeatmapID(){
+
+}
+string Beatmap::getBeatmapsetID(){
+
+}
+float Beatmap::getApproachRate(){
+
+}
+float Beatmap::getCircleSize(){
+
+}
+float Beatmap::getOverallDifficulty(){
+
+}
+float Beatmap::getHPDrainRate(){
+
+}
+float Beatmap::getSliderMultiplier(){
+
+}
+float Beatmap::getSliderTickRate(){
+
+}
+vector<TimingPoint> Beatmap::getTimingPoints(){
+
+}
+vector<std::array<int,3>> Beatmap::getColours(){
+
+}
+vector<HitObject> Beatmap::getHitObjects(){
+
+}
+
+void Beatmap::setOsuFileFormat(int osuFileFormat){
+
+}
+void Beatmap::setAudioFilename(std::string audioFilename){
+
+}
+void Beatmap::setAudioLeadIn(int audioLeadIn){
+
+}
+void Beatmap::setPreviewTime(int previewTime){
+
+}
+void Beatmap::setCountdown(int countdown){
+
+}
+void Beatmap::setSampleSet(std::string sampleSet){
+
+}
+void Beatmap::setStackLeniency(float stackLeniency){
+
+}
+void Beatmap::setMode(int mode){
+
+}
+void Beatmap::setLetterboxInBreaks(int letterboxInBreaks){
+
+}
+void Beatmap::setWidescreenStoryboard(int widescreenStoryboard){
+
+}
+void Beatmap::setBookmarks(vector<int> bookmarks){
+
+}
+void Beatmap::setDistanceSpacing(float distanceSpacing){
+
+}
+void Beatmap::setBeatDivisor(int beatDivisor){
+
+}
+void Beatmap::setGridSize(int gridSize){
+
+}
+void Beatmap::setTimelineZoom(int timelineZoom){
+
+}
+void Beatmap::setTitle(std::string title){
+
+}
+void Beatmap::setTitleUnicode(std::string titleUnicode){
+
+}
+void Beatmap::setArtist(std::string artist){
+
+}
+void Beatmap::setArtistUnicode(std::string artistUnicode){
+
+}
+void Beatmap::setCreator(std::string creator){
+
+}
+void Beatmap::setVersion(std::string version){
+
+}
+void Beatmap::setSource(std::string source){
+
+}
+void Beatmap::setTags(std::string tags){
+
+}
+void Beatmap::setBeatmapID(std::string beatmapID){
+
+}
+void Beatmap::setBeatmapsetID(std::string beatmapsetID){
+
+}
+void Beatmap::setApproachRate(float approachRate){
+
+}
+void Beatmap::setCircleSize(float circleSize){
+
+}
+void Beatmap::setOverallDifficulty(float overallDifficulty){
+
+}
+void Beatmap::setHPDrainRate(float hPDrainRate){
+
+}
+void Beatmap::setSliderMultiplier(float sliderMultiplier){
+
+}
+void Beatmap::setSliderTickRate(float sliderTickRate){
+
+}
+void Beatmap::setTimingPoints(std::vector<TimingPoint> timingPoints){
+
+}
+void Beatmap::setColours(std::vector<std::array<int,3>> colours){
+
+}
+void Beatmap::setHitObjects(std::vector<HitObject> hitObjects){
+
+}
+
+void Beatmap::saveBeatmap(){
+    
+}
